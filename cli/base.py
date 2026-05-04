8839ec2ea7128bee2aacd5e08965c8649f7797c2
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2026-05-04 22:13:37 +0300

    feat: Add delete command and aliases
    - Add 'delete', 'del', 'remove', 'rm' to delete_cmds
    - Implement delete process functionality
    - Add 'rn' alias for 'start' command
    - Add 'nu' alias for 'create' command
    - Add 'lg' alias for 'logs' command

--- cli/base.py
+++ cli/base.py
@@ -12,10 +12,11 @@ class ProcessCMD:
 
         cmd = args[0]
         list_cmds = ["list", "ls"]
-        start_cmds = ["start", "run"]
+        start_cmds = ["start", "run", "rn"]
         stop_cmds = ["stop", "kill"]
-        create_cmds = ["create", "new"]
-        logs_cmds = ["logs", "log"]
+        create_cmds = ["create", "new", "nu"]
+        logs_cmds = ["logs", "log", "lg"]
+        delete_cmds = ["delete", "del", "remove", "rm"]
 
         if cmd in list_cmds:
             self.manager.list_processes()
@@ -49,13 +50,21 @@ class ProcessCMD:
                 return
             name = args[1]
             self.manager.logs(name)
+        elif cmd in delete_cmds:
+            if len(args) < 2:
+                print("[ERROR] Missing process name")
+                return
+            name = args[1]
+            self.manager.delete_process(name)
         else:
             self.print_help()
 
     def print_help(self):
         print("Usage: procm <command>")
         print("Commands:")
-        print("\tcreate, new     - Create a new process")
-        print("\tlist, ls        - List all running processes")
-        print("\tstart, run      - Start a new process")
-        print("\tstop, kill      - Stop a running process")
+        print("\tcreate, new, nu                - Create a new process")
+        print("\tlist, ls                       - List all running processes")
+        print("\tstart, run, rn                 - Start a new process")
+        print("\tstop, kill                     - Stop a running process")
+        print("\tlogs, log, lg                  - View logs of a process")
+        print("\tdelete, del, remove, rm        - Delete a process")
